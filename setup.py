479914aaec7006725635175347416f6ff6b047b2
dangelc <minhdang2032@gmail.com>
2023-09-17 10:53:48 +0700

    chore: update some files

--- setup.py
+++ setup.py
@@ -1,11 +1,16 @@
 from distutils.core import setup
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
     name='GT-Lab-Q2',
     version='0.1dev0',
     author='Team 2', 
-    author_email='author_email@mail.com',
-    packages=find_packages(),
+    author_email='minhdang2032@mail.com, khoavd2003@gmail.com, ntha21122002@gmail.com',
+    packages=['sentence_transformers',
+              'transformers',
+              'psycopg2',
+              'gradio',
+              'torch',
+              'peft'],
     long_description=open('README.md').read()
 )
