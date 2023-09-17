9f5811413f50310b3714c153cb823ee4adc15b78
dangelc <minhdang2032@gmail.com>
2023-09-17 13:13:07 +0700

    chore: update setup.py file

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Gradients-Lab-Q2
+
+Question and Answer model Demo by Team 2
+
+We using Gradio as UI for interact with model
+
--- setup.py
+++ setup.py
@@ -1,16 +1,20 @@
 from distutils.core import setup
-from setuptools import setup
+from setuptools import setup, find_packages
+import os
 
+requirement_path = "requirements.txt"
+install_requires = [] # Here we'll add: ["gunicorn", "docutils>=0.3", "lxml==0.5a7"]
+if os.path.isfile(requirement_path):
+    with open(requirement_path) as f:
+        install_requires = f.read().splitlines()
+
+print(install_requires)
 setup(
     name='GT-Lab-Q2',
     version='0.1dev0',
     author='Team 2', 
     author_email='minhdang2032@mail.com, khoavd2003@gmail.com, ntha21122002@gmail.com',
-    packages=['sentence_transformers',
-              'transformers',
-              'psycopg2',
-              'gradio',
-              'torch',
-              'peft'],
+    url='https://github.com/HugoVox/Gradients-Lab-Q2',
+    packages=install_requires,
     long_description=open('README.md').read()
 )
